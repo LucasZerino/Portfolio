d5165cee1d85e989609f9275909df94ca1e6577f
José Duarte <lucasmatheusaraujo48@gmail.com>
2022-05-06 11:55:04 -0300

    descrição breve do commit

---
+++ src/components/footer/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
+class Footer extends React.Component{
+
+    render() {
+        const FooterMain = styled.div`
+            background-color: #111;
+            height: 50px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        `
+
+        const SocialIcons = styled.div`
+            .social_icon {
+                font-size: 15px;
+                color: #555;
+                margin: 0 5px;
+                cursor: pointer;
+                transition: .5s;
+                &:hover {
+                    color: #04e5e5;
+                }
+            }
+        `
+        return (
+           ''
+        )
+    }
+}
+export default Footer
